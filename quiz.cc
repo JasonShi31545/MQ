f0fa4bcb9673685de8fc08f25bf193833ffe5da2
JasonShi31545Personal <shibingzhao@outlook.com>
2024-01-09 21:10:13 +0800

    Change Makefile, basic data parsing functionality

--- quiz.cc
+++ quiz.cc
@@ -1,8 +1,30 @@
 #include "quiz.h"
+using namespace std;
 
 int main(int argc, const char *argv[]) {
 
     printf("Start Program\n");
+    // std::ifstream f("example.json");
+    // json data = json::parse(f);
+    // using namespace std;
+
+    // cout << data["elapsed"] << endl;
+    // cout << data["items"] << endl;
+    // for (int i = 0; i < data["items"].size(); i++) {
+    //     cout << data["items"][i] << endl;
+    // }
+
+    // cout << data["items"][0]["term"] << endl;
+    // cout << data["items"][0]["def"] << endl;
+    // cout << data["items"][0]["mastery"] << endl;
+    // cout << data["items"][0]["corrects"] << endl;
+    // cout << data["items"][0]["mistakes"] << endl;
+    // cout << data["items"][0]["correct_elapse"] << endl;
+
+    auto res = GetJSONFromFile("example.json");
+
+    cout << res.value()["elapsed"] << endl;
+    cout << res.has_value() << endl;
 
     return 0;
 }
--- quiz.h
+++ quiz.h
@@ -9,10 +9,43 @@
 #include <queue>
 #include <ctime>
 #include <optional>
+#include <string>
+#include <utility>
+#include <any>
+#include "nlohmann/json.hpp"
+#include <fstream>
+#include <typeinfo>
 #include <SDL2/SDL.h>
+using json = nlohmann::json;
 
 #ifndef QUIZ_H
 #define QUIZ_H
+
+class TimeFormatException: public std::exception {
+    public:
+    using std::exception::what;
+    virtual char *what() {
+        return (char *)"Time is incorrect for Set/Item, something has gone wrong in JSON?";
+    }
+};
+
+class DataNotFoundException: public std::exception {
+public:
+    using std::exception::what;
+    virtual char *what() {
+        return message;
+    }
+    DataNotFoundException() {
+        message = (char *)"";
+    }
+    DataNotFoundException(std::string &&str) {
+        message = (char *)str.c_str();
+    }
+
+private:
+    char *message;
+
+};
 
 typedef enum: int {
     TD = 0,
@@ -25,13 +58,13 @@ typedef struct {
     unsigned int mastery;
     unsigned int corrects;
     unsigned int mistakes;
-    std::optional<std::chrono::duration<double>> elapsed_correct;
+    std::optional<std::time_t> elapsed_correct;
 } Item;
 
 struct Set {
     Item *items;
     size_t size;
-    std::optional<std::chrono::duration<double>> elapsed_open;
+    std::optional<std::time_t> elapsed_open;
     Set() {
         items = nullptr;
         size = 0;
@@ -45,7 +78,6 @@ public:
         int correctID;
         Direction direction;
         MCQ() : choices(), correctID(0), direction(TD) {}
-
 };
 class ERQ {
 public:
@@ -55,6 +87,80 @@ public:
 };
 
 
+std::optional<json> GetJSONFromFile(std::string filename) {
+    std::ifstream f(filename);
+    json data;
+    try {
+        data = json::parse(f);
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return std::nullopt;
+    }
+    return data;
+}
 
+std::optional<std::time_t> ParseTimeFromString(std::string st) {
+    // format: "YYYY/MM/DD HH:MM:SS"
+    time_t time;
+    int yy, month, dd, hh, mm, ss;
+    struct tm timestruct;
+    const char *zStart = st.c_str();
+    int ret = sscanf(zStart, "%d/%d/%d %d:%d:%d", &yy, &month, &dd, &hh, &mm, &ss);
+    if (ret == EOF) {
+        return std::nullopt;
+    }
+    timestruct.tm_year = yy;
+    timestruct.tm_mon = month;
+    timestruct.tm_mday = dd;
+    timestruct.tm_hour = hh;
+    timestruct.tm_min = mm;
+    timestruct.tm_sec = ss;
+    timestruct.tm_isdst = -1;
+    time = mktime(&timestruct);
+    std::cout << time << std::endl;
+    return time;
+}
+
+typedef std::pair<std::time_t, std::vector<std::unordered_map<std::string, std::any>>> SetUnwrapped;
+
+SetUnwrapped ParseJSON(json jsdata) {
+    SetUnwrapped res;
+
+    auto time = ParseTimeFromString(jsdata["elapsed-since"]);
+    if (!time.has_value()) throw TimeFormatException();
+
+    if (jsdata.find("items") == jsdata.end()) {
+        throw DataNotFoundException(std::string("Item Not Found"));
+    }
+    for (int i = 0; i < jsdata["items"].size(); i++) {
+        res.second.push_back({
+            {"term", jsdata["items"][i]["term"]},
+            {"def", jsdata["items"][i]["def"]},
+            {"mastery", jsdata["items"][i]["mastery"]},
+            {"corrects", jsdata["items"][i]["corrects"]},
+            {"mistakes", jsdata["items"][i]["corrects"]},
+            {"ces", jsdata["items"][i]["ces"]},
+        });
+    }
+    return res;
+}
+
+Item ParseItem(std::unordered_map<std::string, std::any> itemdata) {
+    Item item;
+    item.term = std::any_cast<std::string>(itemdata["term"]);
+    item.def = std::any_cast<std::string>(itemdata["def"]);
+    item.mastery = std::any_cast<unsigned int>(itemdata["mastery"]);
+    item.corrects = std::any_cast<unsigned int>(itemdata["corrects"]);
+    item.mistakes = std::any_cast<unsigned int>(itemdata["mistakes"]);
+    //item.elapsed_correct = std::any_cast<>(itemdata["term"]);
+    std::optional<std::time_t> t = ParseTimeFromString(std::any_cast<std::string>(itemdata["ces"]));
+    if (!t.has_value()) throw TimeFormatException();
+    item.elapsed_correct = t;
+    return item;
+}
+
+// Set ParseSet(SetUnwrapped setdata) {
+
+// }
 
 #endif // QUIZ_H
