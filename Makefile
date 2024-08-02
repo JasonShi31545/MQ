54b86bd40dcac81872c40ec548e8ba490c71745e
JasonShi31545Personal <shibingzhao@outlook.com>
2024-08-02 21:51:52 +0800

    keyboard

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=clang++
-CPPFLAGS=-std=c++17 -Wall -Wpedantic -Wshadow -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG -O0 -Wno-unused-result -lm -lraylib
+CPPFLAGS=-std=c++17 -Wall -Wpedantic -Wshadow -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG -O0 -Wno-unused-result -lm -lraylib  -I/usr/local/include -L/usr/local/lib
 %: %.cc
 	$(CC) $(CPPFLAGS) $< -o $@.out
 
--- welcome.h
+++ welcome.h
@@ -10,13 +10,36 @@ void display(const float width, const float height) {
     Color colours[4] = {RED, GREEN, YELLOW, BLUE};
     bool rect_hovers[4] = {false, false, false, false};
     int mouseClickID = -1;
+    int keyboardID = -1;
     char a[20] = "";
     int text_x = 0, text_y = 0;
     while (!WindowShouldClose()) {
+        if (IsKeyDown(KEY_Q)) {
+            break;
+        }
         rect1 = {width / 2 - RECT_WIDTH - 5,height / 2, RECT_WIDTH, RECT_HEIGHT - offset};
         rect2 = {width / 2,height / 2, RECT_WIDTH - offset, RECT_HEIGHT};
         rect3 = {width / 2,height / 2 + (RECT_HEIGHT + 5), RECT_WIDTH + offset, RECT_HEIGHT};
         rect4 = {width / 2 - RECT_WIDTH - 5,height / 2 + (RECT_HEIGHT + 5), RECT_WIDTH + offset, RECT_HEIGHT};
+
+        if (IsKeyDown(KEY_ONE) || IsKeyDown(KEY_KP_1)) {
+            keyboardID = 0;
+            sprintf(a, "Keyboard selected %d", keyboardID);
+        }
+        if (IsKeyDown(KEY_TWO) || IsKeyDown(KEY_KP_2)) {
+            keyboardID = 1;
+            sprintf(a, "Keyboard selected %d", keyboardID);
+        }
+
+        if (IsKeyDown(KEY_THREE) || IsKeyDown(KEY_KP_3)) {
+            keyboardID = 2;
+            sprintf(a, "Keyboard selected %d", keyboardID);
+        }
+
+        if (IsKeyDown(KEY_FOUR) || IsKeyDown(KEY_KP_4)) {
+            keyboardID = 3;
+            sprintf(a, "Keyboard selected %d", keyboardID);
+        }
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
